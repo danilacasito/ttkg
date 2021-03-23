67f1232e6f2c3d9abfb3cb7c469c6f44318437b1
daniel <danilacasito8@gmail.com>
2021-03-23 21:07:31 +0100

    First Test Package

---
+++ files/papare/package.py
@@ -0,0 +1,12 @@
+
+import os
+os.system("mkdir -pv /usr/app/papare")
+with open("/usr/app/papare/papare") as f:
+    f.write("#!/usr/bin/python3")
+    f.write("print('Hello World By Papare')")
+e = os.system("ln -sv /usr/app/papare/papare /usr/bin/papare")
+if not e == 0:
+    print("Program returned with code "+str(e))
+    exit(e)
+else:
+    exit(0)
