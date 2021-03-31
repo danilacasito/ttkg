f18b1b93de2a13f0a1cfb6c5ad416862c4afe059
daniel <danilacasito8@gmail.com>
2021-03-31 14:21:24 +0200

    fix some output issues

--- files/nodejs/package.py
+++ files/nodejs/package.py
@@ -6,7 +6,7 @@ def log(text):
     print("[ "+time.asctime()+" ] "+text)
 def start():
     log("Downloading NODE.js version 15.12.0")
-    os.system("wget https://nodejs.org/dist/v15.12.0/node-v15.12.0.tar.gz")
+    os.system("wget https://nodejs.org/dist/v15.12.0/node-v15.12.0.tar.gz > log")
     log("Creating Temporal folder")
     os.system("mkdir temp")
     log("Moving node package to temporaly folder")
@@ -14,7 +14,7 @@ def start():
     log("Entering temporal folder")
     os.chdir("temp")
     log("Unpacking node.js")
-    os.system("tar -xvf nodejs.tar.gz")
+    os.system("tar -xf nodejs.tar.gz")
 
 
 if det == "Windows":
